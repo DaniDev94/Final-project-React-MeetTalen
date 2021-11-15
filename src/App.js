0037285e75ca72910d28acfc66e2af3cf6da7279
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-15 18:18:22 +0100

    FEAT: App changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 import Gallery from './pages/GalleryPage/GalleryPage';
 import Home from './pages/HomePage/Homepage';
+import JobOffer from './pages/JobOfferPage/JobOfferPage';
+import Login from './pages/LoginPage/LoginPage';
+import NewLogin from './pages/LoginPage/NewLoginPage';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
        <Home></Home>
       
        <Gallery></Gallery>
+       <JobOffer></JobOffer>
+       <Login></Login>
+       <NewLogin></NewLogin>
       </header>
     </div>
   );
