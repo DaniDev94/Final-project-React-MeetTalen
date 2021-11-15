664e3642d69d9039f6ed028d6652952e042b2419
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-15 16:25:54 +0100

    FEAT: change to dev

---
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Gallery = () => {
+    return(
+    <>
+        Y aquí vamos a enseñar galeria de todas las ofertas!!!
+    </>
+    )
+}
+export default Gallery
---
+++ src/pages/HomePage/Homepage.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+const Home = () => {
+    return(
+    <>
+        Aquí Aitor va a poner una card jobOffer monísima!!!
+    </>
+    )
+}
+export default Home
