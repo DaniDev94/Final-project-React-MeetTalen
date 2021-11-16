f81e6a9786caeda4063f0b1908dcd489c5c54f3f
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-16 20:59:49 +0100

    FEAT: ok

--- src/pages/JobOfferPage/JobOfferPage.jsx
+++ src/pages/JobOfferPage/JobOfferPage.jsx
@@ -1,48 +1,48 @@
-import React ,{ useState, useEffect} from 'react'
-import { getJobOffers } from '../../api/jobOfferApi'
+import React, { useState, useEffect } from "react";
+import { getJobOffers } from "../../api/jobOfferApi";
 
 const JobOffers = () => {
+  const [jobOffers, setJobOffers] = useState([]);
 
-
-    const [ jobOffers, setJobOffers] = useState([])
-    
-    const getJobOffersApi = async () => {
-        try {
-            const data = await getJobOffers()
-            setJobOffers(data)
-            
-
-        } catch (error) {
-            console.log(error)
-            
-        } 
+  const getJobOffersApi = async () => {
+    try {
+      const data = await getJobOffers();
+      setJobOffers(data);
+    } catch (error) {
+      console.log(error);
     }
-       
-    useEffect (()=>{
-        getJobOffersApi()
-    },[])
-     
+  };
 
+  useEffect(() => {
+    getJobOffersApi();
+  }, []);
 
-
-    return(
+  return (
     <>
-    <h1>joboffer</h1>
-    <button>Crear oferta</button> y que nos navegue a formJobofferPage
-    <div>
-                {jobOffers.map(item => {
-                    console.log(item)
-                    return (
-                        <div key={JSON.stringify(item)}>
-                            <h3>Nombre: {item.jobTitle}</h3>
-                            <button>Crear oferta</button>
-                        </div>
-                 )
-                })}
-                    </div>
+      <h1>Ofertas de trabajo</h1>
+     
+      <div>
+        {jobOffers.map((item) => {
+          console.log(item);
+          return (
+            <div key={JSON.stringify(item)}>
+              <h2> {item.jobInformation.jobTitle}</h2>
+              <p> Vacantes: {item.jobInformation.vacancyNumbers}</p>
+              <p> Idiomas: {item.jobInformation.languages}</p>
+              <p> Sector: {item.jobInformation.sector}</p>
+              <p> Estudios: {item.jobInformation.training}</p>
+              <p> Descripción : {item.jobInformation.jobDescription}</p>
+              <p> Salario : {item.jobInformation.terms.salary}</p>
+              <p> Horas: {item.jobInformation.terms.workingHours}</p>
+              <p> Tipo de contrato: {item.jobInformation.terms.contract}</p>
+              <button>Crear oferta</button>
+            </div>
+            
+          );
+        })}
+      </div>
     </>
-    )
-}
-    
+  );
+};
 
-export default JobOffers
+export default JobOffers;
