4b5f8bb050f3f6acc655356f6e035b15a94b2744
Daniel López <daniel.lp1094@gmail.com>
2021-11-17 11:52:44 +0100

    FEAT: add register and login more logout

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,20 +1,39 @@
 import axios from "axios";
+import { addToken } from "../utils/jwt";
 
+//Alojar la la funcion que recoge el token en una constante, pasarla a la autorización de la cabecera y probar en las rutar que queramos securizar --> console.log(accessToken) = token completo con bearer
+const accessToken = addToken();
 
 const config = {
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
+        'Authorization': `token ${accessToken}`
     }
 }
 
 
 export const registerUser = async (user) => {
     try {
-        const req = await axios.post('http://localhost:4000/users', user, config);
-        return req;
+        const reqInDb = await axios.post('http://localhost:4000/users', user, config);
+        return reqInDb;
     } catch (err) {
         console.log(err);
     }
+}
+
+
+export const loginUser = async (user) => {
+    try {
+        const reqInDb = await axios.post('http://localhost:4000/users/login', user, config);
+        return reqInDb.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Lamas a la función donde quieras para hacer la función logout(añadir en nav)
+export const logoutUser = () => {
+    localStorage.setItem('token', '')
 }
