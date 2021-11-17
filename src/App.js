b16810f48f2ffc1a3579aa7982c8d16a59e9b8ce
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-17 15:38:00 +0100

    Feat: routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Gallery from './pages/GalleryPage/GalleryPage';
-import Home from './pages/HomePage/Homepage';
-import FormJobOffers from './pages/JobOfferPage/FormJobOfferPages';
-import JobOffers from './pages/JobOfferPage/JobOfferPage';
-import Login from './pages/LoginPage/LoginPage';
-import NewLogin from './pages/LoginPage/NewLoginPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import CandidatePage from "./pages/CandidatePage/CandidatePage";
+import Gallery from "./pages/GalleryPage/GalleryPage";
+import Home from "./pages/HomePage/Homepage";
+import FormJobOffers from "./pages/JobOfferPage/FormJobOfferPages";
+import JobOffers from "./pages/JobOfferPage/JobOfferPage";
+import Login from "./pages/LoginPage/LoginPage";
+import NewLogin from "./pages/LoginPage/NewLoginPage";
+import Navbar from "./pages/Navbar/NavbarPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <BrowserRouter>
-       <Home></Home>
-      
-       <Gallery></Gallery>
-       <JobOffers></JobOffers>
-       <FormJobOffers></FormJobOffers>
-       <Login></Login>
-       <NewLogin></NewLogin>
-       </BrowserRouter>
-      </header>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home></Home>} />
+          <Route path="candidates" element ={<CandidatePage></CandidatePage>}/>
+          <Route path="joboffer" >
+          <Route index element={<JobOffers></JobOffers>}/>
+          <Route path="create" element={<FormJobOffers></FormJobOffers>}/>
+          <Route path="#"/>
+          </Route>
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <Route path="login" />
+          <Route element={<Login></Login>} />
+          <Route path="register" />
+          <Route element={<NewLogin></NewLogin>} />
+          <Route path="/joboffer" />
+          <Route element={<JobOffers></JobOffers>} />
+          <Route path="joboffer/create" />
+          <Route element={<FormJobOffers></FormJobOffers>} />
+          <Route path="users/login" element={<Login />} />
+        </Route>
+      </Routes>
+      <Navbar></Navbar>
     </div>
   );
 }
