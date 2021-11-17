56ef363d1895b7700040bf12a6c031d5b01011eb
Daniel López <daniel.lp1094@gmail.com>
2021-11-17 09:40:39 +0100

    "FEAT: delete files without use"

--- src/api/usersApi.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getUsers(){
-    return axios.get("http://localhost:3000/users");
-}
-
-export async function postUser(user){
-    return axios.post("http://localhost:3000/users", user);
-}
