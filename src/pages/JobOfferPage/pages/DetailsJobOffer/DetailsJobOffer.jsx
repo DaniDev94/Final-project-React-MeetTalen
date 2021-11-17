ef53a2e83e9d20a4ae2c54e53acb1300cc915689
Daniel López <daniel.lp1094@gmail.com>
2021-11-17 19:10:59 +0100

    FEAT: create detail job offer page

---
+++ src/pages/JobOfferPage/pages/DetailsJobOffer/DetailsJobOffer.jsx
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom"
+import Navbar from "../../../../components/Navbar/NavbarPage";
+
+
+
+export default function DetailsJobOffer(){
+
+
+    const { id } = useParams();
+    console.log(id);
+    // Crea nuevo metodo http en el back buscando la oferta por id, llamar a la api(oferta) por ese mismo id y settear la oferta que obtenemos pintando los datoas sin.
+    return (
+        <div>Details
+        <Navbar></Navbar>
+        </div>
+    )
+}
