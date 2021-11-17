f64a57b03729855b43164233b6fab4ce375e0d5a
Daniel López <daniel.lp1094@gmail.com>
2021-11-17 19:19:22 +0100

    FEAT: add finished all routes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import CandidatePage from "./pages/CandidatePage/CandidatePage";
 import Home from "./pages/HomePage/Homepage";
-import FormJobOffers from "./pages/JobOfferPage/FormJobOfferPages";
-import JobOffers from "./pages/JobOfferPage/JobOfferPage";
+import FormJobOffers from "./pages/JobOfferPage/pages/FormJobOffer/FormJobOfferPages";
+import LoginPage from "./pages/UsersPage/pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/UsersPage/pages/RegisterPage/RegisterPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import JobOffersPage from "./pages/JobOfferPage/JobOfferPage";
+import DetailsJobOffer from "./pages/JobOfferPage/pages/DetailsJobOffer/DetailsJobOffer";
+
 
 
 function App() {
@@ -15,13 +20,16 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/">
-          <Route index element={<Home></Home>} />
+          <Route index element={<Home></Home>}/>
+          <Route path="login" element={<LoginPage></LoginPage>}/>
+          <Route path="register" element={<RegisterPage></RegisterPage>}/>
+          <Route path="profile" element={<ProfilePage></ProfilePage>}/>
           <Route path="candidates" element={<CandidatePage></CandidatePage>} />
-          <Route path="joboffer" >
-            <Route index element={<JobOffers></JobOffers>} />
+          <Route path="joboffer">
+            <Route index element={<JobOffersPage></JobOffersPage>}/>
             <Route path="create" element={<FormJobOffers></FormJobOffers>} />
+            <Route path=":id" element={<DetailsJobOffer></DetailsJobOffer>} />
           </Route>
-          <Route path="#" />
         </Route>
       </Routes>
     </div>
