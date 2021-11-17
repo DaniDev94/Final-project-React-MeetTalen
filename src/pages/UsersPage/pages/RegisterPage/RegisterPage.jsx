f8a60c2920e88fad520430a8fddeaca57589be44
Daniel López <daniel.lp1094@gmail.com>
2021-11-17 11:55:29 +0100

    FEAT: deleted password repeat

--- src/pages/UsersPage/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/UsersPage/pages/RegisterPage/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { registerUser } from '../../../../api/usersApi';
 import { Password } from 'primereact/password';
@@ -18,7 +18,7 @@ export default function RegisterPage() {
     const addUser = async (user) => {
         try {
             const newUser = await registerUser(user);
-            if(newUser){
+            if (newUser) {
                 console.log('aqui navegaremos a login');
             }
         } catch (err) {
@@ -28,40 +28,41 @@ export default function RegisterPage() {
 
     return (
         <div>
-            <form onSubmit={handleSubmit(submit)}>
-                <div>
-                    <label htmlFor='name'>Nombre de empresa</label>
-                    <input type='text' name='name' {...register('name')} />
-                </div>
-                <div>
-                    <label htmlFor='CEO'>CEO</label>
-                    <input type='text' name='CEO' {...register('CEO')} />
-                </div>
-                <div>
-                    <label htmlFor='CEO_image'>Avatar</label>
-                    <input type='file' name='CEO_image' {...register('image')} />
-                </div>
-                <div>
-                    <label htmlFor='CIF'>CIF</label>
-                    <input type='text' name='CIF' {...register('CIF')} />
-                </div>
-                <div>
-                    <label htmlFor='email'>Email</label>
-                    <input type='text' name='email' {...register('email')} />
-                </div>
-                <div>
-                    <label htmlFor='password'>Contraseña</label>
-                    <Password name='password' promptLabel='Por favor, ingrese su contraseña'  value={password} {...register('password')} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-                <div>
-                    <input type="checkbox" name="privacity" />
-                    <label htmlFor="privacity">Al crear una cuenta, acepta automáticamente todos los términos y condiciones relacionados con meeTTalent</label>
-                </div>
-                <div>
-                    <input type='submit' value='Continuar' />
-                </div>
-            </form>
-
+            <fieldset>
+                <form onSubmit={handleSubmit(submit)}>
+                    <div>
+                        <label htmlFor='name'>Nombre de empresa</label>
+                        <input type='text' name='name' {...register('name')} />
+                    </div>
+                    <div>
+                        <label htmlFor='CEO'>CEO</label>
+                        <input type='text' name='CEO' {...register('CEO')} />
+                    </div>
+                    <div>
+                        <label htmlFor='CEO_image'>Avatar</label>
+                        <input type='file' name='CEO_image' {...register('image')} />
+                    </div>
+                    <div>
+                        <label htmlFor='CIF'>CIF</label>
+                        <input type='text' name='CIF' {...register('CIF')} />
+                    </div>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input type='text' name='email' {...register('email')} />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Contraseña</label>
+                        <Password name='password' promptLabel='Por favor, ingrese su contraseña' value={password} {...register('password')} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" name="privacity" />
+                        <label htmlFor="privacity">Al crear una cuenta, acepta automáticamente todos los términos y condiciones relacionados con meeTTalent</label>
+                    </div>
+                    <div>
+                        <input type='submit' value='Continuar' />
+                    </div>
+                </form>
+            </fieldset>
         </div>)
 }
 
