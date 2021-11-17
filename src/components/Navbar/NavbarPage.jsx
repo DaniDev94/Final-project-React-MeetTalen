3ae5eff820b9dbab8e145325c96274875f82b640
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-17 16:18:11 +0100

    FEAT alvaro's changes

--- src/components/Navbar/NavbarPage.jsx
+++ src/components/Navbar/NavbarPage.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <Link to="/">Home</Link>
+      <Link to="/profile">Perfil</Link>
       <Link to="/candidates">Candidatos</Link>
       <Link to="/joboffer/create">Crear</Link>
       <Link to="/joboffer">Ofertas</Link>
-      <Link to="#">cuenta</Link>
+      
      
     </div>
   );
