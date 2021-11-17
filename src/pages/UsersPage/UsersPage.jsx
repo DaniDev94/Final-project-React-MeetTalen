2b351a22c14e76606f5c7a5bb46eca74f5dd6e70
Daniel López <daniel.lp1094@gmail.com>
2021-11-17 09:28:05 +0100

    FEAT: add resgister page component

---
+++ src/pages/UsersPage/UsersPage.jsx
@@ -0,0 +1,13 @@
+// import React, {useEffect, useState} from "react";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
+
+
+export default function UsersPage() {
+    return (
+        <div>
+        <h1>Users</h1>
+        <RegisterPage></RegisterPage>
+        </div>
+    )
+}
