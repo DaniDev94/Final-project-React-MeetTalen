650941a905b622ad0790843e531ad5401630edc2
Daniel López <daniel.lp1094@gmail.com>
2021-11-17 21:41:13 +0100

    FEAT: add conditional for token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,8 @@
 export const addToken = () => {
     const collectToken = localStorage.getItem('token');
+    if(!collectToken)  {
+        return ''
+    };
     const cleanUpToken = collectToken.replaceAll('"', '');
     const tokenComplet = `Bearer ${cleanUpToken}`;
     return tokenComplet;
