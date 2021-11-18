4b8c80a1082460c43626a81e5286fe06d0876398
Daniel López <daniel.lp1094@gmail.com>
2021-11-18 14:59:02 +0100

    FEAT: add routes auth

--- src/api/jobOfferApi.js
+++ src/api/jobOfferApi.js
@@ -14,46 +14,29 @@ const config = {
         'Authorization': accessToken
     }
 }
-/* export const getJobOffers= () => {
-    try{
-        console.log("estoy entrando" )
-        /* const user = {"user":"6192997c2749fb5610aa239d"} */
-/*  const data= {"user":"6192997c2749fb5610aa239d"}
- const req = axios.post(JOBOFFER,data, config)
- console.log("req", req)
- return req */
-/* }catch(error){
-    console.error(error)
-}
-}  */
+
+
 export const getJobOffers = async () => {
     try {
         const data = { "user": "6192997c2749fb5610aa239d" }
         const req = await fetch('http://localhost:4000/joboffer', {
             method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                'Authorization': accessToken
-            },
+            headers: config.headers,
             body: JSON.stringify(data)
-            
         })
-        const res = await req.json()
-        console.log(res)
-        return res
+        const res = await req.json();
+        console.log(res);
+        return res;
     } catch (error) {
-
-        console.error(error)
+        console.error(error);
     }
 }
 
 
 export const postJobOffers = (jobOffer) => {
     try {
-        const req = axios.post(JOBOFFER, jobOffer, config)
-        return req
+        const req = axios.post(JOBOFFER, jobOffer, config);
+        return req;
     } catch (error) {
         console.error(error)
     }
