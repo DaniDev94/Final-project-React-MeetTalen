747831aeb6d2674699682e842ef7292164c9bddf
Daniel López <daniel.lp1094@gmail.com>
2021-11-18 17:59:22 +0100

    FEAT: clean code return last version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { addToken } from "./utils/jwt";
 import Home from "./pages/HomePage/Homepage";
 import LoginPage from "./pages/UserPages/pages/LoginPage/LoginPage";
@@ -15,6 +15,7 @@ import DetailsJobOffer from "./pages/JobOfferPage/pages/DetailsJobOffer/DetailsJ
 
 
 function App() {
+
   return (
     <div>
       <Routes>
@@ -22,12 +23,12 @@ function App() {
           <Route index element={<Home></Home>}/>
           <Route path="login" element={<LoginPage></LoginPage>}/>
           <Route path="register" element={<RegisterPage></RegisterPage>}/>
-          <Route path="profile" element={addToken() ? <ProfilePage></ProfilePage> : <LoginPage></LoginPage>}/>
-          <Route path="candidates" element={addToken() ? <CandidatePage></CandidatePage> : <LoginPage></LoginPage>} />
+          <Route path="profile" element={ <ProfilePage></ProfilePage>}/>
+          <Route path="candidates" element={<CandidatePage></CandidatePage>} />
           <Route path="joboffer">
-            <Route index element={addToken() ? <JobOffersPage></JobOffersPage> : <LoginPage></LoginPage>}/>
-            <Route path="create" element={addToken() ? <FormJobOffers></FormJobOffers> : <LoginPage></LoginPage>} />
-            <Route path=":id" element={addToken() ? <DetailsJobOffer></DetailsJobOffer> : <LoginPage></LoginPage> } />
+            <Route index element={<JobOffersPage></JobOffersPage>}/>
+            <Route path="create" element={<FormJobOffers></FormJobOffers>} />
+            <Route path=":id" element={<DetailsJobOffer></DetailsJobOffer> } />
           </Route>
         </Route>
       </Routes>
