abfaea6a38de8d27894251f781457b01f8e30641
Daniel López <daniel.lp1094@gmail.com>
2021-11-18 20:50:07 +0100

    FEAT: add logoutRedirect function to logout and redirect to upload page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../../api/usersApi";
 import Navbar from "../../components/Navbar/NavbarPage";
 
 
 export default function ProfilePage() {
-    return(
-        <div>Profilo
-        <Navbar></Navbar>
+
+    const navigate = useNavigate();
+
+    const logoutRedirect = () =>{
+        return logoutUser(navigate('/'));
+    }
+
+    return (
+        <div>
+            <p>Perfil</p>
+            <div>
+                <button onClick={logoutRedirect}>Cerrar Sesion </button> 
+            </div>
+            <Navbar></Navbar>
         </div>
     )
 }
