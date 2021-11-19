c648bbdff9cd0b4dffe546ba12957e303ce29a66
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-19 23:32:26 +0100

    details, con fallo state

--- src/pages/JobOfferPage/pages/DetailsJobOffer/DetailsJobOffer.jsx
+++ src/pages/JobOfferPage/pages/DetailsJobOffer/DetailsJobOffer.jsx
@@ -1,57 +1,43 @@
-import { useParams } from "react-router-dom"
+import React, { useState, useEffect } from "react";
 import Navbar from "../../../../components/Navbar/NavbarPage";
+import { getJobOfferById } from "../../../../api/jobOfferApi"
+import { useParams } from "react-router-dom";
 
 
-
-export default function DetailsJobOffer(){
-
-
+const DetailsJobOffer = () => {
+   
+   
     const { id } = useParams();
-    console.log(id);
-    // Crea nuevo metodo http en el back buscando la oferta por id, llamar a la api(oferta) por ese mismo id y settear la oferta que obtenemos pintando los datoas sin.
+    const [details, setDetails] = useState([]);
+    const getJobOfferId = async () => {
+        try {
+            const data = await getJobOfferById(id);
+            const item = data.data;
+            setDetails(item);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getJobOfferId() ;
+        console.log("estamos en  effect" , details)
+    },[]);
+
     return (
         <>
-        <div>Details
+            <h3>Oferta de trabajo</h3>
+            <h1>{details.jobInformation.jobTitle}</h1>
+ 
+            
 
-<h3>Candidatos</h3>
-        <img src={id.image} alt= "{id.name}"/>
-        <h1>{id.name.first} {id.name.last}</h1>
-        <h2>{id.profession}</h2>
-        <div>
-        <i className="pi-twitter" style={{'fontSize': '2em'}}></i>   
-        <i className="pi-instagram" style={{'fontSize': '2em'}}></i>   
-        <i className="pi-facebook" style={{'fontSize': '2em'}}></i>   
-        </div>
-        <h2>Datos personales</h2>
-        <div>
-        <i className="pi-calendar" style={{'fontSize': '2em'}}></i> 
-        <p>{id.information.age}</p>
-        </div>
-        <div>
-        <i className="pi-map-marker" style={{'fontSize': '2em'}}></i>
-        <p>{id.information.adress.community},{id.information.adress.locality}, {id.information.adress.pc}</p>
-        </div>
-        <div>
-        <i className="pi-phone" style={{'fontSize': '2em'}}></i>
-        <p>{id.information.phone}</p>
-        </div>
 
-        <h2>Palabra clave del perfil</h2>
-        <p>{id.keywords[0]}</p>
-        <p>{id.keywords[1]}</p>
-        <p>{id.keywords[2]}</p>
-        <p>{id.keywords[3]}</p>
-        <p>{id.keywords[4]}</p>
-        <p>{id.keywords[5]}</p>
-        
-        <h2>Formación académica</h2>
-        <p>{id.training.type}</p>
-        <p>{id.training.name}</p>
-        <p>{id.training.center}</p>
 
-        
-        </div>
-        <Navbar></Navbar>
+
+            <Navbar></Navbar>
         </>
     )
 }
+
+export default DetailsJobOffer
