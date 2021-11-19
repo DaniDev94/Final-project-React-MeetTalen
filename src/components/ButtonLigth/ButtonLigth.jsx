2a02f04071fb4fe7b4483e1e72640dd4ec25b4be
Daniel López <daniel.lp1094@gmail.com>
2021-11-19 23:47:40 +0100

    FEAT: Agregar nuevo botón de luz de componente

---
+++ src/components/ButtonLigth/ButtonLigth.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+
+const theme = createTheme({
+    palette: {
+        neutral: {
+            main: '#0d202a',
+            contrastText: '#ffffff',
+        },
+    },
+});
+
+
+export default function ButtonLigth() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Button sx={{padding: '0.8rem 6.5rem', fontWeight: '600', fontSize: '0.8rem'}} color="neutral" variant="contained">
+                Comenzar
+            </Button>
+        </ThemeProvider>
+    )
+}
