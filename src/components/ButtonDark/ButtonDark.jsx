aa2fecfe3b2c9934a33131398edb64772720db97
Daniel López <daniel.lp1094@gmail.com>
2021-11-19 23:37:11 +0100

    FEAT: add new component dark button

---
+++ src/components/ButtonDark/ButtonDark.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+
+const theme = createTheme({
+    palette: {
+        neutral: {
+            main: '#ffffff',
+            contrastText: '#0b4f6e',
+        },
+    },
+});
+
+
+export default function ButtonDark() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Button sx={{padding: '0.8rem 6.5rem', fontWeight: '600', fontSize: '0.8rem'}} color="neutral" variant="contained">
+                Comenzar
+            </Button>
+        </ThemeProvider>
+    )
+}
