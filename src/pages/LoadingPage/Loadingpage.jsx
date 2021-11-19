6a4d00204c0ea0619549ce0d4c61a46f804102f0
Daniel López <daniel.lp1094@gmail.com>
2021-11-19 15:30:13 +0100

    REFACTOR: auto_awesome

--- src/pages/LoadingPage/Loadingpage.jsx
+++ src/pages/LoadingPage/Loadingpage.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loading } from '../../components/Loading/Loading'
 import { Helmet } from 'react-helmet';
+import LoginPage from '../UserPages/pages/LoginPage/LoginPage';
 import './LoadingStyle.scss';
 
 export default function Loadingpage() {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    })
     return (
         <>
             <Helmet>
-                <style>{'body { background-color: #0d202a; }'}</style>
+                {isLoading ? <style>{'body { background-color: #0d202a; }'}</style>
+                    : <style>{'body { background-color: white; }'}</style>}
             </Helmet>
-            <div className='c-loadin'>
-                <div className='c-loadin__load'>
-                    <Loading></Loading>
+            {isLoading ?
+                <div className='c-loadin'>
+                    <div className='c-loadin__load'>
+                        <Loading></Loading>
+                    </div>
+                    <div className='c-loadin__img'>
+                        <img src="images/logo-layout/group7.png" alt="Logo" />
+                    </div>
                 </div>
-                <div className='c-loadin__img'>
-                    <img src="images/logo-layout/group7.png" alt="Logo" />
-                </div>
-            </div>
+                : <LoginPage></LoginPage>}
         </>
     )
 }
