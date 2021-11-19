1c5aa2944dd9e12b9d8c00a7fbc42c42af0811a9
Daniel López <daniel.lp1094@gmail.com>
2021-11-19 13:02:32 +0100

    FEAT: add loading style and finished loading page

--- src/pages/LoadingPage/Loadingpage.jsx
+++ src/pages/LoadingPage/Loadingpage.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { Loading } from '../../components/Loading/Loading'
+import { Helmet } from 'react-helmet';
+import './LoadingStyle.scss';
 
-
-export default function Loadingpage(){
-    return(
-        <div>
-            <Loading></Loading>
-        </div>
+export default function Loadingpage() {
+    return (
+        <>
+            <Helmet>
+                <style>{'body { background-color: #0d202a; }'}</style>
+            </Helmet>
+            <div className='c-loadin'>
+                <div className='c-loadin__load'>
+                    <Loading></Loading>
+                </div>
+                <div className='c-loadin__img'>
+                    <img src="images/logo-layout/group7.png" alt="Logo" />
+                </div>
+            </div>
+        </>
     )
 }
 
