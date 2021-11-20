2402ae9f793f278ce8b17707db83db013fe9f305
Aitor Olivar <aitor.olivar@bootcamp-upgrade.com>
2021-11-20 23:45:36 +0100

    FEAT: style navbar

--- src/components/Navbar/NavbarPage.jsx
+++ src/components/Navbar/NavbarPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BiHomeAlt} from 'react-icons/bi';
+import {BsPeople} from 'react-icons/bs';
+import {AiOutlinePlusSquare} from 'react-icons/ai';
+import {RiShoppingBagLine} from 'react-icons/ri';
+import './NavbarPage.scss';
+
+
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/profile">Perfil</Link>
-      <Link to="/candidates">Candidatos</Link>
-      <Link to="/joboffer/create">Crear</Link>
-      <Link to="/joboffer">Ofertas</Link>
+    <div className='c-containerNavbar'>
+      <Link className='c-linkNav' to="/profile">Perfil <BiHomeAlt className="c-iconNavbar "/> </Link>
+      <Link className='c-linkNav' to="/candidates">Candidatos <BsPeople className="c-iconNavbar"/> </Link>
+      <Link className='c-linkNav' to="/joboffer/create">Crear <AiOutlinePlusSquare className="c-iconNavbar"/> </Link>
+      <Link className='c-linkNav' to="/joboffer">Ofertas <RiShoppingBagLine className="c-iconNavbar"/></Link>
     </div>
   );
 };
 export default Navbar;
+
