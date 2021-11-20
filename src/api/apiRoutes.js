dfcb51beeb6337b8f3d8deb952a75ee776702bcc
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-20 19:43:08 +0100

    change apiRoute

--- src/api/apiRoutes.js
+++ src/api/apiRoutes.js
@@ -1,6 +1,7 @@
 const BASE_URL = "http://localhost:4000";
 const JOBOFFER = `${BASE_URL}/joboffer`;
-const CREATE_JOBOFFER = `${JOBOFFER}/create`
+const CREATE_JOBOFFER = `${JOBOFFER}/create`;
+const JOBOFFER_ID = `${JOBOFFER}/:id`;
 const CANDIDATE = `${BASE_URL}/candidates`;
 const USERS = `${BASE_URL}/users`;
 const LOGIN = `${USERS}/login`;
@@ -10,5 +11,6 @@ export {
     CANDIDATE,
     USERS,
     LOGIN,
-    CREATE_JOBOFFER
+    CREATE_JOBOFFER,
+    JOBOFFER_ID
 };
