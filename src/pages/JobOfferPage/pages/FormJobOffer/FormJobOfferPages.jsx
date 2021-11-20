d548ef58e160074842bc783793e602eb770ba2a0
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-20 19:48:23 +0100

    form ok

--- src/pages/JobOfferPage/pages/FormJobOffer/FormJobOfferPages.jsx
+++ src/pages/JobOfferPage/pages/FormJobOffer/FormJobOfferPages.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { postJobOffers } from "../../../../api/jobOfferApi";
-
-
+import '../FormJobOffer/FormJobOffer.scss'
+import { Helmet } from "react-helmet";
 import Navbar from "../../../../components/Navbar/NavbarPage";
 
 const FormJobOffers = () => {
@@ -15,22 +15,7 @@ const FormJobOffers = () => {
       workingHours: data.workingHours,
       contract: data.contract
     }
-    /* const token = localStorage.getItem('token')
-    const idUser = token.id.toString()
-    console.log(idUser) */
-
-    /* const formData = new FormData();
-    formData.append("jobTitle", data.jobTitle);
-    formData.append("vacancyNumbers", data.vacancyNumbers);
-    formData.append("languages", data.languages);
-    formData.append("sector", data.sector);
-    formData.append("training", data.training);
-    formData.append("jobDescription", data.jobDescription);
-    formData.append("terms.salary", data.salary);
-    formData.append("terms.workingHours", data.workingHours);
-    //formData.append("user",userId) */
-    //addJoboffer(formData);
-    /* console.log(data.jobInformation) */
+    
     let job = {} 
     job.jobInformation= data.jobInformation
     /* job.user= User  */
@@ -52,34 +37,38 @@ const FormJobOffers = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(submit)}>
-        <h2>Descripción</h2>
+    <Helmet>
 
-        <label htmlFor="jobTitle">Oferta:</label>
-        <input type="text" name="jobInformation.jobTitle" {...register("jobInformation.jobTitle")} />
+<style>{'body { background-color: #d9d9db; }'}</style>
 
-        <label htmlFor="languages">Idiomas:</label>
-        <input type="text" name="jobInformation.languages" {...register("jobInformation.languages")} />
+</Helmet>
+      <form className= "c-form" onSubmit={handleSubmit(submit)}>
+        <h2>Descripción de la oferta</h2>
+      
+        <label className = "c-row" htmlFor="jobTitle">Oferta:</label>
+        <input className= "c-input" type="text" name="jobInformation.jobTitle" {...register("jobInformation.jobTitle")} />
 
-        <label htmlFor="vacancyNumbers">Vacantes:</label>
-        <input
+        <label className = "c-row" htmlFor="languages">Idiomas:</label>
+        <input className= "c-input" type="text" name="jobInformation.languages" {...register("jobInformation.languages")} />
+
+        <label  className = "c-row" htmlFor="vacancyNumbers">Vacantes:</label>
+        <input className= "c-input" 
           type="text"
           name="jobInformation.vacancyNumbers"
           {...register("jobInformation.vacancyNumbers")}
         />
 
-        <label htmlFor="sector">Sector:</label>
-        {/* <input type="text" name="jobInformation.sector" {...register("jobInformation.sector")} /> */}
-
-        <select name="jobInformation.sector" {...register("jobInformation.sector")}>
+        <label className = "c-row" htmlFor="sector">Sector:</label>
+        
+        <select className= "c-input"  name="jobInformation.sector" {...register("jobInformation.sector")}>
           <option value =" Diseño Ux/Ui">Diseño Ux/Ui </option>
           <option value ="Desarrollo web">Desarrollo Web</option>
           <option value= "Ciberseguridad">Ciberseguridad</option>
         </select>
 
-        <label htmlFor="training">Estudios:</label>
+        <label className = "c-row" htmlFor="training">Estudios:</label>
         
-        <select name="jobInformation.training" {...register("jobInformation.training")}>
+        <select className= "c-input"  name="jobInformation.training" {...register("jobInformation.training")}>
           <option value ="Grado Medio">Grado Medio</option>
           <option value ="Grado Superior">Grado Superior</option>
           <option value= "Master">Master</option>
@@ -87,21 +76,21 @@ const FormJobOffers = () => {
           <option value= "Sin formación">Sin formación</option>
         </select>
 
-        <label htmlFor="jobDescription">Condiciones laborales:</label>
-        <input
+        <label  className = "c-row" htmlFor="jobDescription">Condiciones laborales:</label>
+        <input className= "c-input" 
           type="text"
           name="jobInformation.jobDescription"
           {...register("jobInformation.jobDescription")}
         />
 
-        <label htmlFor="salary">Salario:</label>
-        <input type="number" name="jobInformation.terms.salary" {...register("jobInformation.terms.salary")} />
+        <label className = "c-row" htmlFor="salary">Salario:</label>
+        <input className= "c-input" type="number" name="jobInformation.terms.salary" {...register("jobInformation.terms.salary")} />
 
-        <label htmlFor="workingHours">Horas:</label>
-        <input type="number" name="jobInformation.terms.workingHours" {...register("jobInformation.terms.workingHours")} />
+        <label className = "c-row" htmlFor="workingHours">Horas:</label>
+        <input className= "c-input" type="number" name="jobInformation.terms.workingHours" {...register("jobInformation.terms.workingHours")} />
 
-        <label htmlFor="contract">Tipo de contrato:</label>
-        <input type="text" name="jobInformation.terms.contract" {...register("jobInformation.terms.contract")} />
+        <label className = "c-row" htmlFor="contract">Tipo de contrato:</label>
+        <input className= "c-input" type="text" name="jobInformation.terms.contract" {...register("jobInformation.terms.contract")} />
 
         <div>
           <input type="submit" value="Enviar" />
