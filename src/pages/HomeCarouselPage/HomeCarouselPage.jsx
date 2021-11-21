46daac2d52edfa22c7332a2c2ef2de6e2d6fc3e8
Daniel López <daniel.lp1094@gmail.com>
2021-11-21 03:32:24 +0100

    RELEASE: finished carousel

--- src/pages/HomeCarouselPage/HomeCarouselPage.jsx
+++ src/pages/HomeCarouselPage/HomeCarouselPage.jsx
@@ -28,7 +28,6 @@ export default function HomeCarouselPage() {
                 <div>
                     <InfoThreePage></InfoThreePage>
                 </div>
-
             </Carousel>
         </>
     )
