de4033dce54b2e4c112267ef18c839fde007a76d
Daniel López <daniel.lp1094@gmail.com>
2021-11-21 02:42:24 +0100

    FEAT: info one page finished

---
+++ src/pages/HomeCarouselPage/pages/InfoOnePage/InfoOnePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './InfoOneStyle.scss';
+import ButtonLigth from '../../../../components/ButtonLigth/ButtonLigth';
+
+export default function InfoOnePage() {
+    return (
+        <>
+            <div className='c-container'>
+                <div className='b-image'>
+                    <img className='b-image__img' src="images/logo-layout/infoOneImg.png" alt="logo" />
+                </div>
+                <div className='b-text'>
+                    <p className='b-text__content'>Meettalent es una herramienta que te ayuda conectar con el mejor talento</p>
+                </div>
+                <div className='b-button'>
+                    <ButtonLigth></ButtonLigth>
+                </div>
+            </div>
+        </>
+    )
+}
