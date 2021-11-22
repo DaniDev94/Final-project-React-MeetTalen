a7cb19a9d14fc2c409b5ecbe2451d6afd40e3b21
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-22 20:30:13 +0100

    formulario styles ok

--- src/pages/JobOfferPage/pages/FormJobOffer/FormJobOfferPages.jsx
+++ src/pages/JobOfferPage/pages/FormJobOffer/FormJobOfferPages.jsx
@@ -4,20 +4,31 @@ import { postJobOffers } from "../../../../api/jobOfferApi";
 import '../FormJobOffer/FormJobOffer.scss'
 import { Helmet } from "react-helmet";
 import Navbar from "../../../../components/Navbar/NavbarPage";
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useNavigate } from "react-router";
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+
+
+
 
 const FormJobOffers = () => {
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
   const submit = (data) => {
     console.log(data.salary)
-    const terms= {
-      salary : data.salary,
+    const terms = {
+      salary: data.salary,
       workingHours: data.workingHours,
       contract: data.contract
     }
-    
-    let job = {} 
-    job.jobInformation= data.jobInformation
+
+    let job = {}
+    job.jobInformation = data.jobInformation
     /* job.user= User  */
 
     console.log(job)
@@ -29,6 +40,7 @@ const FormJobOffers = () => {
       const newJobOffer = await postJobOffers(jobOffer);
       if (newJobOffer) {
         console.log('Oferta creada')
+        navigate('/joboffer')
       }
     } catch (error) {
       console.error(error);
@@ -37,63 +49,96 @@ const FormJobOffers = () => {
 
   return (
     <>
-    <Helmet>
+      <Helmet>
 
-<style>{'body { background-color: #d9d9db; }'}</style>
+        <style>{'body { background-color: #d9d9db; }'}</style>
 
-</Helmet>
-      <form className= "c-form" onSubmit={handleSubmit(submit)}>
-        <h2>Descripción de la oferta</h2>
+      </Helmet>
+      <div className="c-volver">
+
+       
+         
+       
+          <ChevronLeftIcon className="c-volvera" onClick={() => { navigate('/joboffer') }}></ChevronLeftIcon>
+          <h2>Descripción de la oferta</h2>
       
-        <label className = "c-row" htmlFor="jobTitle">Oferta:</label>
-        <input className= "c-input" type="text" name="jobInformation.jobTitle" {...register("jobInformation.jobTitle")} />
+       
+      </div>
 
-        <label className = "c-row" htmlFor="languages">Idiomas:</label>
-        <input className= "c-input" type="text" name="jobInformation.languages" {...register("jobInformation.languages")} />
 
-        <label  className = "c-row" htmlFor="vacancyNumbers">Vacantes:</label>
-        <input className= "c-input" 
-          type="text"
-          name="jobInformation.vacancyNumbers"
-          {...register("jobInformation.vacancyNumbers")}
-        />
+      <form className="c-form" onSubmit={handleSubmit(submit)}>
 
-        <label className = "c-row" htmlFor="sector">Sector:</label>
-        
-        <select className= "c-input"  name="jobInformation.sector" {...register("jobInformation.sector")}>
-          <option value =" Diseño Ux/Ui">Diseño Ux/Ui </option>
-          <option value ="Desarrollo web">Desarrollo Web</option>
-          <option value= "Ciberseguridad">Ciberseguridad</option>
-        </select>
 
-        <label className = "c-row" htmlFor="training">Estudios:</label>
-        
-        <select className= "c-input"  name="jobInformation.training" {...register("jobInformation.training")}>
-          <option value ="Grado Medio">Grado Medio</option>
-          <option value ="Grado Superior">Grado Superior</option>
-          <option value= "Master">Master</option>
-          <option value= "Licenciatura">Licenciatura</option>
-          <option value= "Sin formación">Sin formación</option>
-        </select>
 
-        <label  className = "c-row" htmlFor="jobDescription">Condiciones laborales:</label>
-        <input className= "c-input" 
-          type="text"
-          name="jobInformation.jobDescription"
-          {...register("jobInformation.jobDescription")}
-        />
 
-        <label className = "c-row" htmlFor="salary">Salario:</label>
-        <input className= "c-input" type="number" name="jobInformation.terms.salary" {...register("jobInformation.terms.salary")} />
+        <label className="c-row" htmlFor="oferta"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Oferta" variant="outlined" {...register("jobInformation.jobTitle")} />
+        <label className="c-row" htmlFor="languages"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Idiomas" variant="outlined" {...register("jobInformation.languages")} />
+        <label className="c-row" htmlFor="city"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Ciudad" variant="outlined" {...register("jobInformation.city")} />
+        <label className="c-row" htmlFor="vacancyNumbers"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Vacantes" variant="outlined"  {...register("jobInformation.vacancyNumbers")} />
 
-        <label className = "c-row" htmlFor="workingHours">Horas:</label>
-        <input className= "c-input" type="number" name="jobInformation.terms.workingHours" {...register("jobInformation.terms.workingHours")} />
 
-        <label className = "c-row" htmlFor="contract">Tipo de contrato:</label>
-        <input className= "c-input" type="text" name="jobInformation.terms.contract" {...register("jobInformation.terms.contract")} />
+
+
+        <FormControl sx={{ m: 1.5, minWidth: 220 }}>
+          <InputLabel id="demo-simple-select-label" >Sector</InputLabel>
+          <Select
+            className="c-Text"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Sector"
+
+            name="jobInformation.sector"
+            {...register("jobInformation.sector")}
+
+          >
+            <MenuItem value="Diseño Ux/Ui" >Diseño Ux/Ui</MenuItem>
+            <MenuItem value="Desarrollo Web">Desarrollo Web</MenuItem>
+            <MenuItem value="Ciberseguridad">Ciberseguridad</MenuItem>
+          </Select>
+        </FormControl>
+
+
+
+        <FormControl sx={{ m: 1.5, minWidth: 220 }}>
+          <InputLabel id="demo-simple-select-label" >Estudios</InputLabel>
+          <Select
+            className="c-Text"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Estudios"
+
+            name="jobInformation.training"
+            {...register("jobInformation.training")}
+
+          >
+            <MenuItem value="Grado Medio" >Grado Medio</MenuItem>
+            <MenuItem value="Grado Superior">Grado Superior</MenuItem>
+            <MenuItem value="Grado Superior">Grado Superior</MenuItem>
+            <MenuItem value="Master">Master</MenuItem>
+            <MenuItem value="Licenciatura">Licenciatura</MenuItem>
+            <MenuItem value="Sin formación">Sin formación</MenuItem>
+          </Select>
+        </FormControl>
+
+
+        <label className="c-row" htmlFor="jobDescription"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Condiciones laborales" variant="outlined" {...register("jobInformation.jobDescription")} />
+
+        <label className="c-row" htmlFor="salary"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Salario" variant="outlined"{...register("jobInformation.salary")} />
+
+        <label className="c-row" htmlFor="workingHours"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Horas" variant="outlined"  {...register("jobInformation.workingHours")} />
+
+        <label className="c-row" htmlFor="contract"></label>
+        <TextField className="c-Text" id="outlined-basic" label="Tipo de contrato" variant="outlined" {...register("jobInformation.contract")} />
 
         <div>
-          <input type="submit" value="Enviar" />
+          <input type="submit" value="Continuar" />
         </div>
       </form>
 
