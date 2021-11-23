17e228726acfb4721d528171a9c5d36cb3a92a5c
Aitor Olivar <aitor.olivar@bootcamp-upgrade.com>
2021-11-23 17:47:58 +0100

    FEAT: changes in div

--- src/components/Navbar/NavbarPage.jsx
+++ src/components/Navbar/NavbarPage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BiHomeAlt} from 'react-icons/bi';
-import {BsPeople} from 'react-icons/bs';
-import {AiOutlinePlusSquare} from 'react-icons/ai';
-import {RiShoppingBagLine} from 'react-icons/ri';
+import { BiHomeAlt } from 'react-icons/bi';
+import { BsPeople } from 'react-icons/bs';
+import { AiOutlinePlusSquare } from 'react-icons/ai';
+import { RiShoppingBagLine } from 'react-icons/ri';
 import './NavbarPage.scss';
 
 
@@ -11,10 +11,18 @@ import './NavbarPage.scss';
 const Navbar = () => {
   return (
     <div className='c-containerNavbar'>
-      <Link className='c-linkNav' to="/profile">Perfil <BiHomeAlt className="c-iconNavbar "/> </Link>
-      <Link className='c-linkNav' to="/candidates">Candidatos <BsPeople className="c-iconNavbar"/> </Link>
-      <Link className='c-linkNav' to="/joboffer/create">Crear <AiOutlinePlusSquare className="c-iconNavbar"/> </Link>
-      <Link className='c-linkNav' to="/joboffer">Ofertas <RiShoppingBagLine className="c-iconNavbar"/></Link>
+      <div className="c-containerLink">
+        <Link className='c-linkNav' to="/profile">Perfil <BiHomeAlt className="c-iconNavbar " /> </Link>
+      </div>
+      <div className="c-containerLink">
+        <Link className='c-linkNav' to="/candidates">Candidatos <BsPeople className="c-iconNavbar" /> </Link>
+      </div>
+      <div className="c-containerLink">
+        <Link className='c-linkNav' to="/joboffer/create">Crear <AiOutlinePlusSquare className="c-iconNavbar" /> </Link>
+      </div>
+      <div className="c-containerLink">
+        <Link className='c-linkNav' to="/joboffer">Ofertas <RiShoppingBagLine className="c-iconNavbar" /></Link>
+      </div>
     </div>
   );
 };
